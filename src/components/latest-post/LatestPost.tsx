879b7d0fcb3da2c7c0b14fcdd1a7fb0a99da5e86
Utkarsh Upadhyay <hello@utkupadhyay.com>
2024-02-13 12:21:55 +0530

    Configuring supabase

--- src/components/latest-post/LatestPost.tsx
+++ src/components/latest-post/LatestPost.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, Route, Router, Routes, useNavigate } from 'react-router-dom';
+
 import './LatestPost.scss';
+import { cacheImage } from '../../utils/CacheImage';
+
 interface DataProps {
-  imgURL: string;
+  coverURL: string;
   altTxt: string;
   blogURL: string;
   postedOn: string;
@@ -9,15 +13,35 @@ interface DataProps {
   author: string;
   content: string;
   title: string;
+  id: string;
 }
+
 const LatestPost = ({ data }: any) => {
-  const postData = data.slice(0, 2);
-  console.log({ postData });
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState<Boolean>(true);
+  const postData = data.length > 1 ? data.slice(0, 2) : data;
+  useEffect(() => {
+    if (data.length > 0) {
+      setLoading(false);
+    }
+  }, [data]);
+  const handleImageClick = async (id: string) => {
+    const post = data.find((p: DataProps) => p.id === id);
+    if (post && post.coverURL) {
+      await cacheImage(post.coverURL, 'cache-first');
+      navigate(`/blog/${id}`);
+    }
+  };
+
   const latestPostImageContainer = postData?.map(
     (post: DataProps, i: number) => {
-      return post?.imgURL !== '' ? (
-        <picture>
-          <img src={post.imgURL} />
+      return post?.coverURL !== '' ? (
+        <picture key={i} onClick={() => handleImageClick(post.id)}>
+          <img
+            src={post.coverURL}
+            className="post-img-size"
+            alt={post.altTxt}
+          />
         </picture>
       ) : (
         <div className="post-img-container" key={i}></div>
@@ -25,6 +49,15 @@ const LatestPost = ({ data }: any) => {
     }
   );
 
-  return <div className="blog-article-meta">{latestPostImageContainer}</div>;
+  return (
+    <div className="blog-article-meta">
+      {isLoading ? (
+        <div className="post-img-container"></div>
+      ) : (
+        latestPostImageContainer
+      )}
+    </div>
+  );
 };
+
 export default LatestPost;
