5538035e8e57e130c7ddb16c0fd223fe1943fe31
Utkarsh Upadhyay <utkarsh@Utkarshs-MacBook-Pro-2.local>
2024-03-17 11:57:22 +0530

    Update vite file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
